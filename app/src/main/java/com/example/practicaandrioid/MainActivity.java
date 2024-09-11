bb955038c2cc63f0a6dc47139bea87c41b31bce6
FlorenciaCastelucci <florcastelucci97@gmail.com>
2024-09-11 20:16:48 -0300

    Cambios

--- app/src/main/java/com/example/practicaandrioid/MainActivity.java
+++ app/src/main/java/com/example/practicaandrioid/MainActivity.java
@@ -1,24 +1,65 @@
 package com.example.practicaandrioid;
 
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements  NavigationView.OnNavigationItemSelectedListener{
+    DrawerLayout drawerLayout;
+    ImageButton buttonDrawerToggle;
+    NavigationView navigationView;
+
+    private static final int ID_PERFIL = R.id.navPerfil;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.drawer_layout), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        buttonDrawerToggle = findViewById(R.id.buttonDrawerToggle);
+        navigationView = findViewById(R.id.navigationView);
+
+
+        buttonDrawerToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.open();
+            }
+        });
+
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+
     }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item){
+        selectItemNav(item);
+        return true;
+    }
+
 }
